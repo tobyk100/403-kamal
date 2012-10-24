1ae4fae7a5b8cb3ddbbbe998437d60cf6cfcadf7
Brandon Bond <bond13@cs.washington.edu>
2012-10-24 16:40:38 -0700

    updating feed

--- instafeed/instafeed/urls.py
+++ instafeed/instafeed/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'mainapp.views.index', name='index'),
     url(r'^signup/', 'mainapp.views.signup', name='signup'),
+    url(r'^feed/', 'mainapp.views.feed', name='feed'),
     url(r'^twitter_request/', 'mainapp.views.twitter_request', name='twitter_request'),
     url(r'^facebook_request/', 'mainapp.views.facebook_request', name='fb_request'),
     # url(r'^instafeed/', include('instafeed.foo.urls')),
--- instafeed/mainapp/views.py
+++ instafeed/mainapp/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from emailusernames.forms import EmailUserCreationForm
 from django.contrib.auth import authenticate, login
 
+def feed(request):
+  return render(request, 'Feed.html')
+
 def index(request):
   return render(request, 'index.html')
 
