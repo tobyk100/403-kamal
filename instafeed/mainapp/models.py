7c8842c1b6fcdef00b09531f5359077c91f11348
Tobias Kahan <tobyk100@gmail.com>
2012-10-25 12:38:33 -0700

    Updated models.py, seems to work

--- instafeed/mainapp/models.py
+++ instafeed/mainapp/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Account(models.Model):
   user_id = models.ForeignKey(User)
   access_token = models.CharField(max_length=255)
-  expires = models.DateTimeField()
   
   class Meta:
-    abstract = true;
+    abstract = True;
 
 class FacebookAccount(Account):
+  expires = models.DateTimeField()
 
 class TwitterAccount(Account):
   access_secret = models.CharField(max_length=255)
+  expires = models.DateTimeField()
