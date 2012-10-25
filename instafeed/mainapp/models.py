b3832c73b864d41af243ab10396e5f0e7755d4de
Tobias Kahan <tobyk100@gmail.com>
2012-10-25 11:42:25 -0700

    Build first instance of account models

--- instafeed/mainapp/models.py
+++ instafeed/mainapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+  user_id = models.ForeignKey(User)
+  access_token = models.CharField(max_length=255)
+  expires = models.DateTimeField()
+  
+  class Meta:
+    abstract = true;
+
+class FacebookAccount(Account):
+
+class TwitterAccount(Account):
+  access_secret = models.CharField(max_length=255)
