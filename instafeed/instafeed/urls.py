b5d9fc6fd210fa582d00fb6529261e1ed68bc44b
acetang <acetang@uw.edu>
2012-10-26 16:36:04 -0700

    add login view

--- instafeed/instafeed/urls.py
+++ instafeed/instafeed/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'mainapp.views.index', name='index'),
     url(r'^signup/', 'mainapp.views.signup', name='signup'),
+    url(r'^login/', 'mainapp.views.login', name='login'),
     url(r'^feed/', 'mainapp.views.feed', name='feed'),
     url(r'^feed/twitter_request/', 'mainapp.views.twitter_request', name='twitter_request'),
     url(r'^facebook_request/', 'mainapp.views.facebook_request', name='fb_request'),
