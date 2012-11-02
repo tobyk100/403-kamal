2c66ebf9675fc052d61de53cc47366b99a4f26da
Tobias Kahan <tobyk100@uw.edu>
2012-11-02 12:40:48 -0700

    added facebook_signin url to urls

--- instafeed/instafeed/urls.py
+++ instafeed/instafeed/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     url(r'^feed/twitter_request/', 'mainapp.views.twitter_request', name='twitter_request'),
     url(r'^facebook_request/', 'mainapp.views.facebook_request', name='fb_request'),
     url(r'^twitter_signin/', 'mainapp.views.twitter_signin', name='twitter_signin'),
+    url(r'^facebook_signin/', 'mainapp.views.facebook_signin', name='facebook_signin'),
     #FIXME may need to update this regex
     url(r'^twitter_callback', 'mainapp.views.twitter_callback', name='twitter_callback'),                   
     # url(r'^instafeed/', include('instafeed.foo.urls')),
