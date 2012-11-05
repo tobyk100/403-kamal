de1577a04ae992b4f2169298c72f706e1499b6ff
Brandon Bond <bond13@cs.washington.edu>
2012-11-04 18:34:49 -0800

    changed parser

--- instafeed/mainapp/static/js/FacebookParse.js
+++ instafeed/mainapp/static/js/FacebookParse.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     alert(document.URL);
-    var string = Document.URL;
+    var string = document.URL;
     var array = string.split("#");
     var token = array[1].split("&");
     alert(token[0]);
