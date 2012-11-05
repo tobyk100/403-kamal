a242ccaf6ed32289104d43a2d1325a517e8cc301
Brandon Bond <bond13@cs.washington.edu>
2012-11-04 18:39:29 -0800

    parser

--- instafeed/mainapp/static/js/FacebookParse.js
+++ instafeed/mainapp/static/js/FacebookParse.js
@@ -4,4 +4,16 @@ window.onload = function () {
     var array = string.split("#");
     var token = array[1].split("&");
     alert(token[0]);
+    var accessToken = token[0].split("=");
+    
+    $.ajax({
+        type: "POST",
+        url: "/facebook_access/",
+        data: { token: accessToken[1] },
+        datatype: "json",
+        error: function (data) { alert('Error:' + data); },
+        success: function (data) {
+	    $(location).attr('href',"http://dry-peak-6840.herokuapp.com/accounts/");
+        }
+    }); 
 }
