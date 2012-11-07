0d8933718eebc040998b7f314b9365715fb70f63
Tobias Kahan <tobyk100@gmail.com>
2012-11-07 13:25:27 -0800

    created settings file for twitter

---
+++ instafeed/mainapp/twitter_lib.py
@@ -0,0 +1,8 @@
+from settings import DEBUG
+
+if DEBUG:
+  consumer_key="NTuzgYZY3fCMMQcIDn73Xg"
+  consumer_secret="9NAvbXWoZktTQYb9BHTQnMtleNq6UBJXlbsDUWJY"
+else:
+  consumer_key="FL6V9vrWlfVKzpFVB0iDmg"
+  consumer_secret="Td8Mn4BzStppzPNDblylAibTXmDfRt0gjzN1cFAI"
