4804cc5b93aebc178fcaf7a32552fd3ca306c7ba
Tobias Kahan <tobyk100@gmail.com>
2012-11-07 13:46:53 -0800

    added basic request to google view

--- instafeed/mainapp/google_views.py
+++ instafeed/mainapp/google_views.py
@@ -1,7 +1,11 @@
+from django.shortcuts import redirect
 from django.http import HttpResponse
 import json, os
 
 def google_signin(request):
-  response = {}
-  response['message'] = os.environ['SERVER_SOFTWARE']
-  return HttpResponse(json.dumps(response), mimetype="application/json")
+  params = {}
+  params['response_type'] = 'code'
+  params['client_id'] = '138535205339.apps.googleusercontent.com'
+  params['redirect_id'] = 'http://dry-peak-6840.herokuapp.com/google_callback'
+  params['scope'] = 'https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&'
+  return redirect('http://accounts.google.com/o/oauth2/auth', params)
