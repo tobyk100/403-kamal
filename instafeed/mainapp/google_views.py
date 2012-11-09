daa01d6ff24ab3727c7f8cd73a3f5191565ae71f
Tobias Kahan <tobyk100@gmail.com>
2012-11-08 20:31:24 -0800

    Added some code to google_views, added tests

--- instafeed/mainapp/google_views.py
+++ instafeed/mainapp/google_views.py
@@ -2,22 +2,54 @@ from django.http import HttpResponse, HttpResponseRedirect
 import json
 import google_api
 
+"""
+Main use case flows:
+    1) Ask for users' posts
+    2) Check session for valid token
+    3) If valid, use to get posts and return
+    4) If not valid or non existent check database for refresh token
+    5) If refresh token, use to get access token, cache access token in session
+      get posts, return
+    6) If no refresh token, go to signin
+
+    1) Ask to signin user
+    2) Check for valid refresh token in database
+    3) If valid refresh token then return success (user is signed in)
+    4) If no valid refresh token then redirect to google
+    5) if callback fails return fail
+    6) if callback succeeds, put refresh token into db, get access token, cache
+      access token into session.
+"""
+
 def google_signin(request):
+  response = {}
+  if not request.user.is_authenticated():
+    response['success'] = False
+    response['message'] = "User not authenticated"
+    return HttpResponse(json.dumps(response))
+
+  account = GoogleAccount.get_account(request.user)
+  if account is None:
+    redirect = _request_refresh_token(request)
+    redirect['success'] = True
+    redirect['message'] = "User redirected to G+ for signin"
+    return redirect
+
+  response['refresh_token'] = account.refresh_token
+  return HttpResponse(json.dumps(response))
+
+def google_request_token(request, refresh_token):
+  account = GoogleAccount.get_account(request.user)
+  refresh_token = _request_refresh_token(request) if not account else \
+                  account.refresh_token
+
+def google_get_posts(request):
   response = {}
   token = request.session.get('google_token')
   if (token is None) or (not is_valid(token)):
     token = google_request_token(request)
 
-  return HttpResponse(json.dumps(response))
-
-def google_request_token(request):
-  user = request.user
-  account = GoogleAccount.get_account(user)
-  refresh_token = _request_refresh_token(request) if not account else \
-                  account.refresh_token
-
-def google_get_posts(request):
-  pass
+  return HttpResponse(json.dumps(response), 'application/json')
 
 
 def google_request_code(request):
--- instafeed/mainapp/tests.py
+++ instafeed/mainapp/tests.py
@@ -5,12 +5,32 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.utils import unittest
+from mainapp.models import GoogleAccount
+from django.http import HttpResponse, HttpRequest
+from django.test.client import Client
+import json
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class GoogleTest(TestCase):
+  def setUp(self):
+    user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+    user.save()
+
+  def test_google_model(self):
+    user = User.objects.get(id=1)
+    account = GoogleAccount(user_id=user, access_token="test_access_token", 
+        refresh_token="test_refresh_token")
+    account.save()
+    account = GoogleAccount.get_account(user.id)
+    self.assertEqual("test_access_token", account.access_token)
+    self.assertEqual("test_refresh_token", account.refresh_token)
+
+  def test_google_signin_not_authenticated(self):
+    c = Client()
+    response = c.post('/google_signin/')
+    json_response = json.loads(response.content)
+    self.assertEqual(json_response["success"], False)
