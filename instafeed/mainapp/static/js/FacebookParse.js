fdd1eff3af56d90921880c13d64dd9cb29612e61
Brandon Bond <bond13@cs.washington.edu>
2012-11-13 10:18:30 -0800

    changed redirect

--- instafeed/mainapp/static/js/FacebookParse.js
+++ instafeed/mainapp/static/js/FacebookParse.js
@@ -13,7 +13,7 @@ window.onload = function () {
         datatype: "json",
         error: function (data) { alert('Error:' + data); },
         success: function (data) {
-	    $(location).attr('href', "https://dry-peak-6840.herokuapp.com/accounts/");
+	    $(location).attr('href', "/accounts/");
         }
     }); 
 }
