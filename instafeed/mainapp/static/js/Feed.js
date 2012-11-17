80ba1008994be0bf1d9f8518fa972ee79b8add0d
Brandon Bond <bond13@cs.washington.edu>
2012-11-16 16:48:50 -0800

    turned off automatic update

--- instafeed/mainapp/static/js/Feed.js
+++ instafeed/mainapp/static/js/Feed.js
@@ -1,14 +1,14 @@
 //Binds all appropriate buttons with clicks
 $(document).on('ready', function() {
     $('#submitPostButton').on('click', submitPost);
-    $('#facebookRefreshButton').bind('click', loadFacebookFeed());
-    $('#twitterRefreshButton').bind('click', loadTwitterFeed());
-    $('#googleRefreshButton').bind('click', loadGoogleFeed());
-    var refreshId = setInterval(function(){
-	loadFacebookFeed();
-	loadTwitterFeed();
-	loadGoogleFeed();
-    }, 60000);
+    $('#facebookRefreshButton').bind('click', loadFacebookFeed);
+    $('#twitterRefreshButton').bind('click', loadTwitterFeed);
+    $('#googleRefreshButton').bind('click', loadGoogleFeed);
+    //var refreshId = setInterval(function(){
+//	loadFacebookFeed();
+//	loadTwitterFeed();
+//	loadGoogleFeed();
+  //  }, 60000);
     
 });
 
@@ -201,3 +201,4 @@ function urlify(text) {
         return '<a href="' + url + '">' + url + '</a>';
     });
 }
+
