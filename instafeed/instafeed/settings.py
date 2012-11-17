45e985d151a8d3ed1f920cb68434e8fac78d34d0
acetang <acetang@uw.edu>
2012-11-16 17:09:18 -0800

    Setting up password recovery

--- instafeed/instafeed/settings.py
+++ instafeed/instafeed/settings.py
@@ -1,4 +1,10 @@
 # Django settings for instafeed project.
+import os
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
