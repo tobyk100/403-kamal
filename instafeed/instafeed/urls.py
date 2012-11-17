982afd7090deb23c73b270a8883653bd2eb327a4
acetang <acetang@uw.edu>
2012-11-16 17:15:44 -0800

    Added password_reset_done.html

--- instafeed/instafeed/urls.py
+++ instafeed/instafeed/urls.py
@@ -14,7 +14,8 @@ urlpatterns = patterns('',
         {'template_name': 'password_reset_form.html', \
          'email_template_name': 'password_reset_email.html'}, \
         name = 'password_reset'),
-    url(r'^password_reset_done/', 'django.contrib.auth.views.password_reset_done', name = 'password_reset_done'),
+    url(r'^password_reset_done/', 'django.contrib.auth.views.password_reset_done', \
+        {'template_name': 'password_reset_done.html'}),
     url(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
     url(r'^password_reset_complete/', 'django.contrib.auth.views.password_reset_complete', name = 'password_reset_complete'),
     url(r'^feed/', 'mainapp.views.feed', name='feed'),
