75f3474cb633cb2f4928b4edb0e7ead4930ec53f
rcemmett <ryancemmett@gmail.com>
2012-11-27 11:41:09 -0800

    more script stuff

--- instafeed/mainapp/scheduled_updates_script.py
+++ instafeed/mainapp/scheduled_updates_script.py
@@ -1,5 +1,5 @@
 from models import TwitterAccount, FacebookAccount, Account, ScheduledUpdates 
-import datetime
+import datetime, twitter_views, facebook_views
 
 def CheckForUpdates():
   now = datetime.datetime.now()
@@ -10,4 +10,10 @@ def CheckForUpdates():
 		publish_date__month=now.month,
 		publish_date__day=now.day)
   for post in posts:
-    #TODO for each post that has been found for the day we need to make a twitter/facebook request	  
+    #TODO for each post that has been found for the day we need to make a twitter/facebook request	 
+    if post.publish_site is 1 or 3:
+      fb_account = FacebookAccount.get_account(post.user_id)
+      facebook_api.facebook_post_feed(post.update, fb_account.access_token)  
+    if post.publish_site is 2 or 3:
+      twitter_account = TwitterAccount.get_account(post.user_id)
+      twitter_api.twitter_post(twitter_account.access_token, twitter_account.access_secret, post.update)   
