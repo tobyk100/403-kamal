2851cd1b378a6ddc03a8cd9bfac45a6c51564c6c
rcemmett <ryancemmett@gmail.com>
2012-11-29 23:52:22 -0800

    script updates

--- instafeed/mainapp/management/commands/scheduled_updates.py
+++ instafeed/mainapp/management/commands/scheduled_updates.py
@@ -9,12 +9,13 @@ class Command(BaseCommand):
     now = datetime.datetime.now()
     #we can now call now.year, now.hour,  now.month, and now.day to figure out the exact date
     #querying the db returns datetime objects
+    #TODO need to learn to query these date times correctly. For some reason giving me an error on hour
     posts = ScheduledUpdates.objects.filter(publish_date__year=now.year,
                                             publish_date__month=now.month,
-                                            publish_date__day=now.day,
-                                            publish_date__hour=now.hour)
+                                            publish_date__day=now.day)
+                                            #publish_date__hour=now.hour)
     for post in posts:
-      #TODO for each post that has been found for the day we need to make a twitter/facebook request
+      
       if post.publish_site is 1 or 3:
         fb_account = FacebookAccount.get_account(post.user_id)
         facebook_api.facebook_post_feed(post.update, fb_account.access_token)
