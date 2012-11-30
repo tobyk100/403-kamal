21edc92c52ab7071a81a36743e068b57e41a4225
rcemmett <ryancemmett@gmail.com>
2012-11-29 23:44:54 -0800

    fixed script

---
+++ instafeed/mainapp/management/commands/scheduled_updates.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+from mainapp.models import TwitterAccount, FacebookAccount, Account, ScheduledUpdates
+import datetime, mainapp.twitter_views, mainapp.facebook_views
+
+
+class Command(BaseCommand):
+
+  def handle(self, *args, **options):
+    now = datetime.datetime.now()
+    #we can now call now.year, now.hour,  now.month, and now.day to figure out the exact date
+    #querying the db returns datetime objects
+    posts = ScheduledUpdates.objects.filter(publish_date__year=now.year,
+                                            publish_date__month=now.month,
+                                            publish_date__day=now.day,
+                                            publish_date__hour=now.hour)
+    for post in posts:
+      #TODO for each post that has been found for the day we need to make a twitter/facebook request
+      if post.publish_site is 1 or 3:
+        fb_account = FacebookAccount.get_account(post.user_id)
+        facebook_api.facebook_post_feed(post.update, fb_account.access_token)
+      if post.publish_site is 2 or 3:
+        twitter_account = TwitterAccount.get_account(post.user_id)
+        twitter_api.twitter_post(twitter_account.access_token, twitter_account.access_secret, post.update)   
