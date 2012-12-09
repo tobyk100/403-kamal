0c35c1c49b23b9a3299cc3cadfe72183d69518ec
Tobias Kahan <tobyk100@gmail.com>
2012-12-08 17:00:02 -0800

    fixed logc check

--- instafeed/mainapp/management/commands/scheduled_updates.py
+++ instafeed/mainapp/management/commands/scheduled_updates.py
@@ -2,12 +2,13 @@ from django.core.management.base import BaseCommand, CommandError
 from mainapp.models import TwitterAccount, FacebookAccount, Account, ScheduledUpdates
 import datetime
 from mainapp import twitter_api, twitter_views
+import pytz
 
 
 class Command(BaseCommand):
 
   def handle(self, *args, **options):
-    now = datetime.datetime.utcnow()
+    now = pytz.UTC.localize(datetime.datetime.utcnow())
     #we can now call now.year, now.hour,  now.month, and now.day to figure out the exact date
     #querying the db returns datetime objects
     #TODO need to learn to query these date times correctly. For some reason giving me an error on hour
@@ -16,13 +17,12 @@ class Command(BaseCommand):
                                             publish_date__day=now.day)
                                             #publish_date__hour=now.hour)
     
-    
     for post in posts:
       post_delta = datetime.timedelta(0, 600, 0)
       #if post.publish_site is 1 or 3:
       #  fb_account = FacebookAccount.get_account(post.user_id)
       #  facebook_api.facebook_post_feed(post.update, fb_account.access_token)
-      if post.publish_site is 2 or 3 and (now - post.publish_date < post_delta):
+      if (post.publish_site is 2 or 3) and ((now - post.publish_date) < post_delta):
         twitter_account = TwitterAccount.get_account(post.user_id)
         twitter_api.twitter_post(twitter_account.access_token, twitter_account.access_secret, post.update)
         post.posted = True
